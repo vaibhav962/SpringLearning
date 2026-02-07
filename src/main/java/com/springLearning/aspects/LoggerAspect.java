0bf2e4ce2052e4e223dfe938fa430c3a2decbb10
Vaibhav Chauhan <chauhanvaibhav9602@gmail.com>
2026-02-07 17:08:37 -0500

    configuring @AfterThrowing and @AfterReturning advice

--- src/main/java/com/springLearning/aspects/LoggerAspect.java
+++ src/main/java/com/springLearning/aspects/LoggerAspect.java
@@ -1,13 +1,14 @@
 package com.springLearning.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Aspect
@@ -24,5 +25,15 @@ public class LoggerAspect {
         long timeElapsed = Duration.between(start, finish).toMillis();
         logger.info("Time took to execute the method : "+timeElapsed);
         logger.info(joinPoint.getSignature().toString() + " method execution end");
+    }
+
+    @AfterThrowing(value = "execution(* com.springLearning.services.*.*(..))",throwing = "e")
+   public void logException(JoinPoint joinPoint, Exception e){
+        logger.log(Level.SEVERE, joinPoint.getSignature() + " An exception was thrown with the help of @AfterThrowing which happened due to: "+e.getMessage());
+   }
+
+    @AfterReturning(value = "execution(* com.springLearning.services.*.*(..))",returning = "val")
+    public void logStatus(JoinPoint joinPoint, Object val){
+        logger.log(Level.INFO, joinPoint.getSignature() + " Method successfully processed with the status of: "+ val.toString());
     }
 }
